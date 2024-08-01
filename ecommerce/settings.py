cd6c3489a3833ef9acea2ef8c317dbf8cb29c226
MohamedAbdElMoniedBadr <mahamedatlath@gmail.com>
2024-08-01 18:45:18 +0300

    some additions

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground'
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templetes'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- playground/views.py
+++ playground/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse ,HttpResponseNotAllowed
 # Create your views here.
 def say_hello(request):
-    return HttpResponse("hello tamola")
+    return render(request , "home.html")
 def not_allowed(request):
     return HttpResponse("<h1 style = 'color:red'>no tamola allow</h1>")
